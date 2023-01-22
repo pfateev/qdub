0f5b19608fa81b5396fa282c751d2fe5e5b34570
Pavel Fateev <pavel.m.fateev@gmail.com>
2023-01-22 12:05:02 -0800

    add simple get

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,19 @@ import rectangle132 from "./assets/rectangle132.svg";
 import shape from "./assets/shape.svg";
 import Button from "./components/Button";
 import Input from "./components/Input";
+
 const App = () => {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.message);
+        setData(data.message);
+      });
+  }, []);
+
   const propsData = {
     input: {
       enterYourFullName: "First Name\n",
@@ -22,6 +34,9 @@ const App = () => {
       <span className="queue-prototype">Queue prototype</span>
       <span className="manual-student-ta-enq">
         Manual student&#x2F;TA enqueue-ing for prototype
+      </span>
+      <span className="manual-student-ta-enq">
+        {!data ? "Loading..." : data}
       </span>
       <Input className="input-instance-1" {...propsData.input} />
       <Input className="input-1-instance" {...propsData.input1} />
