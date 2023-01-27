add605405d85102a25719cc3e994a7d3fd4ce481
Wendi Ma <wma5@cs.washington.edu>
2023-01-27 14:40:04 -0800

    edited front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,35 @@
 import * as React from "react";
 import "./App.css";
-import rectangle132 from "./assets/rectangle132.svg";
 import shape from "./assets/shape.svg";
 import Button from "./components/Button";
 import Input from "./components/Input";
 const App = () => {
+  const [checked, setChecked] = React.useState(false);
+  const handleChange = () => {
+    setChecked(!checked);
+  };
+
   const propsData = {
     input: {
-      enterYourFullName: "First Name\n",
+      enterYourFullName: "First Name",
     },
     input1: {
       enterYourFullName: "Last Name",
     },
     button: {
-      logIn: "Log In!",
+      logIn: "Sign up!",
     },
   };
+
+  const Checkbox = ({ label, value, onChange }) => {
+    return (
+      <label>
+        <input type="checkbox" checked={value} onChange={onchange} />
+        {label}
+      </label>
+    )
+  }
+
   return (
     <div className="registration">
       <img className="shape" src={shape} />
@@ -23,11 +37,22 @@ const App = () => {
       <span className="manual-student-ta-enq">
         Manual student&#x2F;TA enqueue-ing for prototype
       </span>
-      <Input className="input-instance-1" {...propsData.input} />
-      <Input className="input-1-instance" {...propsData.input1} />
+      <form action="signup.html" method="post" id="signup">
+        <div class="field">
+          <label for="firstname">First Name:</label>
+          <Input type="text" id="firstname" name="firstname" placeholder="Enter your first name" />
+        </div>
+        <div class="field">
+          <label for="lastname">Last Name:</label>
+          <Input type="text" id="lastname" name="lastname" placeholder="Enter your last name" />
+        </div>
+      </form>
       <div className="flex-container">
-        <img className="rectangle-132" src={rectangle132} />
-        <span className="are-you-a-ta">Are you a TA?</span>
+        <Checkbox
+          label="Are you a TA?"
+          checked={checked}
+          onChange={handleChange}
+        />
       </div>
       <Button className="button-instance-1" {...propsData.button} />
     </div>
