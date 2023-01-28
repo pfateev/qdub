eb81e47019ca3b99ee4d06f563f8e13092af1ac2
Wendi Ma <wma5@cs.washington.edu>
2023-01-28 14:46:12 -0800

    fixed css of registration page

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,10 @@
 import * as React from "react";
 import "./App.css";
-import shape from "./assets/shape.svg";
-import Button from "./components/Button";
-import Input from "./components/Input";
 import RegistrationForm from "./components/RegistrationForm";
 
 const App = () => {
-  const [checked, setChecked] = React.useState(false);
-  const handleChange = () => {
-    setChecked(!checked);
-  }
-  
-
-  const propsData = {
-    input: {
-      enterYourFullName: "First Name",
-    },
-    input1: {
-      enterYourFullName: "Last Name",
-    },
-    button: {
-      logIn: "Sign up!",
-    },
-  };
-
-  const Checkbox = ({ label, value, onChange }) => {
-    return (
-      <label>
-        <input type="checkbox" checked={value} onChange={onchange} />
-        {label}
-      </label>
-    );
-  };
-
   return (
-    <div className="registration">
-      <RegistrationForm />
-      <img className="shape" src={shape} />
-      <span className="queue-prototype">Queue prototype</span>
-      <span className="manual-student-ta-enq">
-        Manual student&#x2F;TA enqueue-ing for prototype
-      </span>
-      <form action="signup.html" method="post" id="signup">
-        <div class="field">
-          <label for="firstname">First Name:</label>
-          <Input type="text" id="firstname" name="firstname" placeholder="Enter your first name" />
-        </div>
-        <div class="field">
-          <label for="lastname">Last Name:</label>
-          <Input type="text" id="lastname" name="lastname" placeholder="Enter your last name" />
-        </div>
-      </form>
-      <div className="flex-container">
-        <Checkbox
-          label="Are you a TA?"
-          checked={checked}
-          onChange={handleChange}
-        />
-      </div>
-      <Button className="button-instance-1" {...propsData.button} />
-    </div>
+    <RegistrationForm />
   );
 };
 export default App;
--- client/src/components/RegistrationForm.jsx
+++ client/src/components/RegistrationForm.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import shape from "../assets/shape.svg";
+import "./RegistrationForm.css";
 
 export default function MyForm() {
   const [firstName, setFirstName] = useState('');
@@ -15,17 +17,23 @@ export default function MyForm() {
   };
 
   return (
-    <div>
+    <div className="registration">
+      <img className="logo" src={shape} alt="top right circles"/>
+      <span className="title">Queue prototype</span>
+      <span className="description">
+        Manual student&#x2F;TA enqueue-ing for prototype
+      </span>
       <label>
-        First Name: <input value={firstName} onChange={e => setFirstName(e.target.value)} />
+        First Name: <input class="input" value={firstName} placeholder="Enter your first name" onChange={e => setFirstName(e.target.value)} />
       </label>
       <label>
-        Last Name: <input value={lastName} onChange={e => setLastName(e.target.value)} />
+        Last Name: <input class="input" value={lastName} placeholder="Enter your last name" onChange={e => setLastName(e.target.value)} />
       </label>
       <label>
-        Are you a TA?: <input type="checkbox" name="isTA" />
+        <input class="checkBox" type="checkbox" name="isTA" /> Are you a TA?
       </label>
-      <button type="submit" onClick={submitForm}>Submit</button>
+
+      <button class="button" type="submit" onClick={submitForm}>Sign up!</button>
     </div>
   );
 }
