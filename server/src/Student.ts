a935355477a5e3cca0071cac312d76b235c1936d
Pavel Fateev <pavel.m.fateev@gmail.com>
2023-02-02 13:07:14 -0800

    add name field to Student class

--- server/src/Student.ts
+++ server/src/Student.ts
@@ -1,18 +1,24 @@
 export class Student {
-      id: number; 
+      id: number;
       pos: number;
       time: number;
       qtime: number;
       status: boolean;
       question: string;
+      name: string;
 
-      constructor(id: number, pos: number, time: number, status: boolean, qtime: number, question: string) {
+      constructor(id: number, pos: number,
+            time: number, status: boolean,
+            qtime: number, question: string,
+            name: string) {
+
             this.id = id; // ID of the query associated with student's uwNetID
             this.pos = pos; // Position of query in queue
             this.time = time; // Time estimation of how long to resolve query
             this.status = status; // Status of student 
             this.qtime = qtime; //Estimated time length until student can get helped
             this.question = question; // Question of the query
+            this.name = name;
       }
 
       // getter methods:
@@ -26,7 +32,7 @@ export class Student {
       public getTime(): number {
             return this.time;
       }
-      
+
       public getQTime(): number {
             return this.qtime;
       }
@@ -39,13 +45,17 @@ export class Student {
             return this.question;
       }
 
+      public getName(): string {
+            return this.name;
+      }
+
       // setter methods:
       public setPos(pos: number): void {
             this.pos = pos;
       }
       public setTime(time: number): void {
             this.time = time;
-      } 
+      }
 
       public setStatus(b: boolean): void {
             this.status = b;
@@ -59,4 +69,4 @@ export class Student {
 }
 
 
- export default Student;
+export default Student;
