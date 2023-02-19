97ae9cacccc61faebbb4d6b415e59a8c4f8eca6b
Jared Tran <jtran9@cs.washington.edu>
2023-02-19 12:22:45 -0800

    added notifcation functions, activate functions, and reset

--- server/src/DoublyLinkedList.ts
+++ server/src/DoublyLinkedList.ts
@@ -9,12 +9,14 @@ class DoublyLinkedList
 
    private size: number;
    private timelen: number;
+   private message: string | null;
 
    constructor() {
        this.head = null;
        this.tail = null;
        this.size = 0;
        this.timelen = 0;
+       this.message = null;
    }
 
     public getSize(): number {
@@ -265,6 +267,15 @@ class DoublyLinkedList
             }
             curr = curr.next;
         }
+    }
+
+    //Ta Message to students
+    public setMessage(message: string): void{
+        this.message = message;
+    }
+
+    public getMessage(): string | null {
+        return this.message;
     }
     //    We don't need these 
 //    public getFirst(): any
