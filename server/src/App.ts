e68fd0e538bc6e8b4dc243ea35282222d6428270
Tri Vuong <trivuong14@gmail.com>
2023-02-19 15:21:06 -0800

    Add fake db

--- server/src/App.ts
+++ server/src/App.ts
@@ -12,22 +12,121 @@ app.use(express.json());
 app.use(cors());
 // app.options('*', cors());
 
-// Course object for beta release
-let course = new Course(403, "Software Engineering");
-const registrationDatabase: Map<string, Student> = new Map();
-
-// Student and ta objects for beta release
-const ids: number[] = [11111, 11112, 11113, 11114, 11115];
-const names: string[] = ["Student1", "Student2", "Student3", "Student4", "Student5"]
-const questions: string[] = ["question1", "question2", "question3", "question4", "question5"];
-const questionTime = 10;
-let waitTime = 0;
-
-for (let i = 0; i < 5; i++) {
-    const student = new Student(ids[i], i + 1, questionTime, true, waitTime, questions[i], names[i])
-    // waitTime += questionTime;
-    course.enqueue(student);
+export interface IHash {
+	[details: string] : Set<number>;
 }
+
+export interface IHash2 {
+	[details: number] : Course;
+}
+
+/* FAKE DB
+ 							COURSE TABLE 
+	----------------------------------------------
+	courseId 							courseName
+	----------------------------------------------
+	403										Software Engineer
+	455										Computer Vision 
+
+							STUDENT TABLE
+	----------------------------------------------
+	NetID 				Name 					Password? 
+	----------------------------------------------
+	izzyv 				Izzy					
+	wenli					Wendy
+	pashap				Pasha
+	jaredt				Jared
+	triv					Tri	
+	stu1					Student1
+	stu2					Student2
+	stu3					Student3
+	stu4					Student4
+
+								COURSE-STUDENT TABLE
+	----------------------------------------------
+	courseID				NetID
+	----------------------------------------------
+	403							izzyv
+	403							wenli
+	403							stu1
+	403							stu2
+	455							pashap
+	455							triv
+	455 						stu3
+	455							stu4
+
+
+									COURSE-TA TABLE
+	----------------------------------------------
+	courseID				NetID
+	----------------------------------------------
+	403							jaredt
+	403							pashap
+	455							wenli
+	455 						izzyv
+
+	Map<courseID, course object> 
+	Map<NetID, Set<courseID>> studentClassesMap
+	Map<NetID, Set<courseID>> taClassesMap 
+ */
+
+
+	let courseId = [403, 455];
+	let courseName = ["Software Engineer", "Computer Vison"];
+	let students = ["izzyv", "wenli", "pashap", "jaredt", "triv", "stu1", "stu2", "stu3", "stu4"];
+	let course403 = ["stu", "stu", "ta", "ta", "none", "stu", "stu", "none", "none"];
+	let course455 = ["ta", "ta", "stu", "none", "stu", "none", "none", "stu", "stu"];
+
+	let studentClassesMap: IHash = {};
+	let taClassesMap: IHash = {};
+	let courseMap: IHash2 = {};
+
+	// Build course object and map them with courseID in courseMap
+	for (let i = 0; i < courseId.length; i++) {
+		const course = new Course(courseId[i], courseName[i]);
+		courseMap[courseId[i]] = course;
+	}
+
+	// Build studentClassesMap and taClassesMap
+	for (let i = 0; i < students.length; i++) {
+		if(course403[i] == "stu") {
+			if (studentClassesMap[students[i]]) {
+				studentClassesMap[students[i]].add(403);
+			} else {
+				let studentClasses =new Set<number>;
+				studentClasses.add(403);
+				studentClassesMap[students[i]] = studentClasses;
+			}
+		} else if (course403[i] == "ta") {
+			if (taClassesMap[students[i]]) {
+				taClassesMap[students[i]].add(403);
+			} else {
+				let taClasses =new Set<number>;
+				taClasses.add(403);
+				taClassesMap[students[i]] = taClasses;
+			}
+		}
+
+		if(course455[i] == "stu") {
+			if (studentClassesMap[students[i]]) {
+				studentClassesMap[students[i]].add(455);
+			} else {
+				let studentClasses =new Set<number>;
+				studentClasses.add(455);
+				studentClassesMap[students[i]] = studentClasses;
+			}
+		} else if (course455[i] == "ta") {
+			if (taClassesMap[students[i]]) {
+				taClassesMap[students[i]].add(455);
+			} else {
+				let taClasses =new Set<number>;
+				taClasses.add(455);
+				taClassesMap[students[i]] = taClasses;
+			}
+		}
+	}
+
+
 // TODO: allow one student/TA to register
 app.post("/students", (req, res) => {
     console.log(req.body);
