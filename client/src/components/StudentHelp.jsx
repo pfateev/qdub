5b8165d2dae17a5811f59b1e03239b7b7530b714
Wendi Ma <wma5@cs.washington.edu>
2023-03-01 21:48:49 -0800

    fixed /ta-view and /student-view style

--- client/src/components/StudentHelp.jsx
+++ client/src/components/StudentHelp.jsx
@@ -1,6 +1,7 @@
 import "./GeneralStyle.css"
 import circles from "../assets/circles.png";
 import dog from "../assets/questionDog.png";
+import { Box } from '@chakra-ui/react'
 
 export const StudentHelp = ({ currQuestion }) => {
 
@@ -9,11 +10,19 @@ export const StudentHelp = ({ currQuestion }) => {
     <div>
       <img className="logo" src={circles} alt="top left circles" />
         <div className="webpage">
-        <h1 className="title">Your Turn!</h1>
+        <h1 className="title" style={{ marginBottom: '5%' }}>Your Turn!</h1>
         <img className="dog" src={dog} alt="cute dog" />
-        <p className="description">
-          {currQuestion}
-        </p>
+        <Box mt='5%' borderColor='RGBA(0, 0, 0, 0.16)'
+              borderRadius='15px'
+              borderWidth='2px'
+              w='40%' minH='8rem'
+              p='3rem'
+              textAlign="center"
+          >
+            <p className="description">
+              {currQuestion}
+            </p>
+          </Box>
       </div>
     </div>
   );
